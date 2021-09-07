f40e0237f6aa3d4e038b77de0ed12d236d766569
kalunge <muthomititus+github@gmail.com>
2021-09-07 15:53:01 +0100

    Add post comment function

--- src/addComment.js
+++ src/addComment.js
@@ -1,12 +1,12 @@
 const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8WhiXHgGMaGrsfo6vYsR/comments';
 
-const addComment = async () => {
+const addComment = async (itemId, username, comment) => {
   const response = await fetch(url, {
     method: 'post',
     body: JSON.stringify({
-      item_id: 'item100',
-      username: 'titoh',
-      comment: 'great item',
+      itemId,
+      username,
+      comment,
     }),
     headers: {
       'Content-Type': 'application/json',
