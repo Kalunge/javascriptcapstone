88dcdace5050c6741f460f33ada2eefb8fe9d96b
kalunge <muthomititus+github@gmail.com>
2021-09-07 14:13:23 +0100

    Fix linter errors

--- src/comments.js
+++ src/comments.js
@@ -1,24 +1,27 @@
+import meal from './meal.jpg';
+
 const modalBody = document.querySelector('.modal-body');
 const mealName = 'My Meal';
-const title = 'Comments(3)';
-const oldComments = ['my commentr', 'your comment', "I love the african cuisine"];
-import { comment } from 'postcss';
-import meal from './meal.jpg';
+const oldComments = [
+  'my commentr',
+  'your comment',
+  'I love the african cuisine',
+];
 
 const addModal = () => {
   const img = document.createElement('img');
   img.src = meal;
   const p = document.createElement('h2');
-  p.classList.add('name')
+  p.classList.add('name');
   p.innerHTML = mealName;
 
   modalBody.appendChild(img);
-  const div= document.createElement('div')
-  div.classList.add('items')
+  const div = document.createElement('div');
+  div.classList.add('items');
   div.appendChild(p);
-  const headingFour = document.createElement('h4')
-  headingFour.innerHTML = `comments (${oldComments.length})`
-  div.appendChild(headingFour)
+  const headingFour = document.createElement('h4');
+  headingFour.innerHTML = `comments (${oldComments.length})`;
+  div.appendChild(headingFour);
   oldComments.forEach((comment) => {
     const p = document.createElement('p');
     p.innerHTML = comment;
