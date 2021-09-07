a03351d243b09236a950a62c40357fd5c03febf9
kalunge <muthomititus+github@gmail.com>
2021-09-07 16:05:15 +0100

    Resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import './styles.css';
-<<<<<<< HEAD
-import addModal from './comments.js';
-=======
 import getMeals from './getmeals.js';
 import homeList from './homelist.js';
 import addModal from './comments.js';
 
->>>>>>> dev
-
 window.addEventListener('load', async () => {
-  (homeList(await getMeals()));
+  homeList(await getMeals());
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   addModal();
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> dev
